aa3407a2b8b48f9786e37aae35f82cd17e1e8161
Urwashi10 <142391363+Urwashi10@users.noreply.github.com>
2026-04-30 21:14:38 +0530

    Update 02_get_employees_by_salary_range.sql

--- 01_stored_procedures/02_get_employees_by_salary_range.sql
+++ 01_stored_procedures/02_get_employees_by_salary_range.sql
@@ -1,1 +1,13 @@
+--Code
+CREATE PROCEDURE get_employees_by_salary_range
+    @min_salary INT,
+    @max_salary INT
+AS
+BEGIN
+    SELECT id, name, salary, department_id
+    FROM employees
+    WHERE salary BETWEEN @min_salary AND @max_salary;
+END;
 
+--execution
+EXEC get_employees_by_salary_range 30000, 60000;
