c317aa17a9da5a0548e10b0d9f5f12f7ccd540e6
Urwashi10 <142391363+Urwashi10@users.noreply.github.com>
2026-04-30 21:26:24 +0530

    Update 02_after_update_trigger.sql

--- 03_triggers/02_after_update_trigger.sql
+++ 03_triggers/02_after_update_trigger.sql
@@ -1,1 +1,10 @@
+-- Trigger after updating employee
 
+CREATE TRIGGER trg_after_update_employee
+ON employees
+AFTER UPDATE
+AS
+BEGIN
+    SELECT * FROM INSERTED; -- new data
+    SELECT * FROM DELETED;  -- old data
+END;
