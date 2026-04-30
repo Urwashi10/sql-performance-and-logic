d056dc32667502c0c6ee03974876cc8470439fc5
Urwashi10 <142391363+Urwashi10@users.noreply.github.com>
2026-04-30 21:22:42 +0530

    Update 01_scalar_add_bonus.sql

--- 02_functions/01_scalar_add_bonus.sql
+++ 02_functions/01_scalar_add_bonus.sql
@@ -1,1 +1,11 @@
+-- Add 10% bonus to salary
 
+CREATE FUNCTION add_bonus (@salary DECIMAL(10,2))
+RETURNS DECIMAL(10,2)
+AS
+BEGIN
+    RETURN @salary * 1.10;
+END;
+
+-- Usage
+SELECT dbo.add_bonus(50000);
