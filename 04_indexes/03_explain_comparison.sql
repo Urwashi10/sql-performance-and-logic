cde1e03b935f337c3809c865083c93a4bc877e00
Urwashi10 <142391363+Urwashi10@users.noreply.github.com>
2026-04-30 21:31:06 +0530

    Update 03_explain_comparison.sql

--- 04_indexes/03_explain_comparison.sql
+++ 04_indexes/03_explain_comparison.sql
@@ -1,1 +1,11 @@
+-- Query before index
 
+EXPLAIN
+SELECT * FROM employees WHERE name = 'John';
+
+-- Create index
+CREATE NONCLUSTERED INDEX idx_name ON employees(name);
+
+-- Query after index
+EXPLAIN
+SELECT * FROM employees WHERE name = 'John';
