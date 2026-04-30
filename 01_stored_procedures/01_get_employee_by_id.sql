67daf3ed595a13150d518297d32a7f3c2d12a9bb
Urwashi10 <142391363+Urwashi10@users.noreply.github.com>
2026-04-30 21:12:11 +0530

    Update 01_get_employee_by_id.sql

--- 01_stored_procedures/01_get_employee_by_id.sql
+++ 01_stored_procedures/01_get_employee_by_id.sql
@@ -1,1 +1,10 @@
+CREATE PROCEDURE get_employee_by_id
+    @emp_id INT
+AS
+BEGIN
+    SELECT id, name, salary, department_id
+    FROM employees
+    WHERE id = @emp_id;
+END;
 
+EXEC get_employee_by_id 1;
