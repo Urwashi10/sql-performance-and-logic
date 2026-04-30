edd09986baa4745723306d505c4f541fe3c2b631
Urwashi10 <142391363+Urwashi10@users.noreply.github.com>
2026-04-30 21:32:56 +0530

    Update 01_acid_concepts.sql

--- 05_transactions/01_acid_concepts.sql
+++ 05_transactions/01_acid_concepts.sql
@@ -1,1 +1,18 @@
+-- ACID Properties
 
+-- Atomicity: All or nothing
+-- Consistency: Data remains valid
+-- Isolation: Transactions don’t interfere
+-- Durability: Changes are permanent after commit
+
+-- Example structure
+
+BEGIN TRANSACTION;
+
+-- SQL operations here
+
+COMMIT;
+
+-- or
+
+ROLLBACK;
